4988e73bcba260f8e9193a6bee241d80bb2ea0bd
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2025-10-28 10:23:57 +0100

    initial commit with swagger

---
+++ src/main/java/ma/inpt/tp4_api/Tp4ApiApplication.java
@@ -0,0 +1,13 @@
+package ma.inpt.tp4_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/inpt/tp4_api/Tp4ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.inpt.tp4_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
